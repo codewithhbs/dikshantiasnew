d0c12abaaf6d917e9f7d86ac6cd7094286cd9e24
Rahul Rai <therahulray@gmail.com>
2025-09-02 12:47:43 +0530

    change text editor add new text editor

--- src/component/admin/RichTextEditor.tsx
+++ src/component/admin/RichTextEditor.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { Editor } from "@tinymce/tinymce-react";
-import { useRef } from "react";
+import dynamic from "next/dynamic";
+import { useState } from "react";
+
+const JoditEditor = dynamic(() => import("jodit-react"), { ssr: false });
 
 export default function RichTextEditor({
   value,
@@ -10,45 +12,21 @@ export default function RichTextEditor({
   value: string;
   onChange: (content: string) => void;
 }) {
-  const editorRef = useRef(null);
+  const [content, setContent] = useState(value || "");
 
   return (
     <div className="w-full">
-      <Editor
-        apiKey="nem2benxflhzoqa897i6yv6u492wfl4d5xpj09u6c1rroe7o" // free mode (no account needed)
-        onInit={(_evt, editor) => (editorRef.current = editor)}
-        value={value}
-        onEditorChange={(newValue) => onChange(newValue)}
-        init={{
+      <JoditEditor
+        value={content}
+        onChange={(newContent) => {
+          setContent(newContent);
+          onChange(newContent);
+        }}
+        config={{
           height: 300,
-          menubar: true,
-          plugins: [
-            "advlist",
-            "autolink",
-            "lists",
-            "link",
-            "image",
-            "charmap",
-            "preview",
-            "anchor",
-            "searchreplace",
-            "visualblocks",
-            "code",
-            "fullscreen",
-            "insertdatetime",
-            "media",
-            "table",
-            "code",
-            "help",
-            "wordcount",
-          ],
-          toolbar:
-            "undo redo | blocks | " +
-            "bold italic forecolor backcolor | alignleft aligncenter " +
-            "alignright alignjustify | bullist numlist outdent indent | " +
-            "removeformat | image media link table | code fullscreen preview | help",
-          content_style:
-            "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+          readonly: false,
+          toolbarAdaptive: false,
+          toolbarSticky: false,
         }}
       />
     </div>
