855e1b5ab5958e745a76d01508b83c3d3caad574
anish-stack <anishjha896@gmail.com>
2025-09-08 17:56:34 +0000

    update

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: "dikshantias",
+      script: "npm",
+      args: "start",
+      cwd: "/root/dikshantiasnew",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
