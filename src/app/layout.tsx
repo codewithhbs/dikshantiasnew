5e79dd71b2426d9c112d9f7f01697cae7e3c8f92
codewithhbs <hbsdevelopersteam@gmail.com>
2025-10-13 18:11:20 +0530

    add gtm code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import ClientLayoutWrapper from "@/component/ClientLayoutWrapper";
 import Providers from "./providers";
 import { Suspense } from "react";
 import { Toaster } from "react-hot-toast"; // ✅ import Toaster
+import Script from "next/script";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
@@ -17,6 +18,19 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+
+      <head>
+        <meta name="google-site-verification" content="c7FgyR8QTNRAU7VO6riBBz8M8JYhKXKQa11Q8Bn5CN4" />
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-ZB3WCMNJ4D"></Script>
+      <Script id="google-analytics">
+      {`  window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-ZB3WCMNJ4D');`}
+      </Script>
+
+      </head>
       <body>
         <Providers>
           <Suspense fallback={<div>Loading...</div>}>
